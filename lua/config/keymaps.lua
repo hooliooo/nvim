4a899bd4dac1fc07f40dfadef00722d587450f83
Julio Alorro <alorro3@gmail.com>
2025-06-01 23:13:37 +0200

    Added harpoon

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+
 local telescope_builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", telescope_builtin.find_files, { desc = "Find files via Telescope" })
 vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, { desc = "Telescope Live Grep" })
