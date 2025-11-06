b10a485876f04ef80e0904bae1b99738eabaa734
Julio Alorro <alorro3@gmail.com>
2025-11-06 20:30:05 +0100

    Stuff

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
+    opts.ensure_installed = opts.ensure_installed or {}
     -- Add new parsers to ensure_installed without overwriting
     vim.list_extend(opts.ensure_installed, {
-      "rust",
+      "elixir",
+      "heex",
+      "eex",
       "kotlin",
+      "rust",
       "swift",
     })
   end,
